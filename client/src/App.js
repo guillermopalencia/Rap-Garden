ba7752c04109baef990790005c2fce2e87b54729
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-28 19:50:22 -0400

    Created home component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg'
 import './App.css'
 import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">{/* probably nav here */}</header>
       <main>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
     </div>
   )
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+  return (
+    <div>
+      <h1> Enter The Rap Garden </h1>
+      <p> One Stop Query For Everything Rap </p>
+    </div>
+  )
+}
+
+export default Home
