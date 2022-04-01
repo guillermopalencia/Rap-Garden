ff02d7972925ac72402f824079e4d570b01f9d10
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-31 20:47:59 -0400

    shining light function added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,15 +10,13 @@ const Home = () => {
   return (
     <div className="Home">
       <h1 className="click" onClick={() => page()}>
-        {' '}
-        Enter The Rap Garden{' '}
+        Enter The Rap Garden
       </h1>
       <br></br>
       <br></br>
       <p>
-        {' '}
-        You've been formally invited to the Rap Garden <br></br> Enter to find
-        all your rap needs{' '}
+        You've been cordially invited to the Rap Garden <br></br> Enter to find
+        all your rap needs
       </p>
     </div>
   )
