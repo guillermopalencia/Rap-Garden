d3c145f25d635baba44b8fc7fff5dd3fd9c16ad1
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-31 20:28:17 -0400

    added onclick function to home page that brings you to the garden

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,25 @@
+import { useNavigate } from 'react-router-dom'
+
 const Home = () => {
+  let navigate = useNavigate()
+
+  const page = () => {
+    navigate('/artists')
+  }
+
   return (
     <div className="Home">
-      <h1 className="click"> Enter The Rap Garden </h1>
-      <p> One Stop Query For Everything Rap </p>
+      <h1 className="click" onClick={() => page()}>
+        {' '}
+        Enter The Rap Garden{' '}
+      </h1>
+      <br></br>
+      <br></br>
+      <p>
+        {' '}
+        You've been formally invited to the Rap Garden <br></br> Enter to find
+        all your rap needs{' '}
+      </p>
     </div>
   )
 }
