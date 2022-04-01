ed27dfa88694b39eae82df01906d3e956c6a4a26
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-31 20:20:56 -0400

    styled nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,8 +4,12 @@ const Header = () => {
   return (
     <header>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/artists">Artists</Link>
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/artists">
+          Artists
+        </Link>
       </nav>
     </header>
   )
